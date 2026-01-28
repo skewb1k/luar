6fd602076281375996220825f86daea30a20f323
skewb1k <skewb1kunix@gmail.com>
2026-01-28 23:30:22 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Luar
+
+Luar is a Lua linter written in Zig.
+
+At the moment, only Lua 5.1 is planned to be supported.
+
+**Work in progress**
---
+++ build.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "luar",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
+}
